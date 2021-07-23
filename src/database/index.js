9301473b15a30da4ffafa74a28cb6d5299848d2b
larts85 <lianel.sotolongo@reanswer.com.ar>
2021-07-23 19:46:34 -0300

    Adding babel presets Modularizing the code Character and Episode schemas created Added resolvers for: -character -characters -episodes -episode

---
+++ src/database/index.js
@@ -0,0 +1,43 @@
+import fetch from "node-fetch";
+
+export const fetchEpisodes = () => {
+  return fetch("https://rickandmortyapi.com/api/episode/")
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      return json;
+    })
+    .then((json) => json.results);
+};
+
+export const fetchEpisodeById = (id) => {
+  return fetch("https://rickandmortyapi.com/api/episode/" + id)
+    .then((res) => res.json())
+    .then((json) => json);
+};
+
+export const fetchEpisodeByUrl = (url) => {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((json) => json);
+};
+
+export const fetchCharacters = () => {
+  return fetch("https://rickandmortyapi.com/api/character/")
+    .then((res) => res.json())
+    .then((json) => {
+      return json.results;
+    });
+};
+
+export const fetchCharacterById = (id) => {
+  return fetch("https://rickandmortyapi.com/api/character/" + id)
+    .then((res) => res.json())
+    .then((json) => json);
+};
+
+export const fetchCharacterByUrl = (url) => {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((json) => json);
+};
---
+++ src/graphql/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require("apollo-server");
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+
+export const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+
+  type Query {
+    characters: [Character]
+    episodes: [Episode]
+    character(id: ID!): Character
+    episode(id: ID!): Episode
+  }
+
+  type Character {
+    id: ID
+    name: String
+    status: String
+    spicies: String
+    type: String
+    gender: String
+    image: String
+    episode: [String]
+  }
+
+  type Episode {
+    id: ID
+    name: String
+    air_date: String
+    episode: String
+    characters: [Character]!
+    created: String
+  }
+`;
